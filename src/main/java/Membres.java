95f143f8dc494769754e7698d6c24a99a09e5048
Andres.K <andreskpognon@gmail.com>
2025-05-07 09:50:37 +0200

    Commit intial Avec convertion du fichier membres_APPLI en dic

---
+++ src/main/java/Membres.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class Membres {
+    public static void main(String[] args) throws FileNotFoundException {
+        File memeberliste = new File("pokemon_appli_data/membres_APPLI.txt");
+        Scanner scan = new Scanner(memeberliste);
+        Dictionary memberdic = new Hashtable();
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] split = line.split(" ");
+            memberdic.put(split[0], split[1]);
+        }
+        System.out.println(memberdic);
+    }
+}
