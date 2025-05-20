2ba7459aabe261bb99b494520a857e34b85075c6
Andres <andreskpognon@gmail.com>
2025-05-20 20:15:59 +0200

    modif

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +15,29 @@ public class Main {
         } catch (FileNotFoundException e) {
             System.out.println("Fichier non trouvé.");
         }*/
+
+
+        //Test méthode lireDistances
+        /* 
+
+        try {
+            ArrayList<ArrayList<String>> distances = VoisinsTab.lireDistances("pokemon_appli_data/distances.txt");
+            System.out.println(distances);
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichier non trouvé.");
+        }*/
+
+        //Test méthode lireScenario
+        /* 
+
+        try {
+            Map<String, String> scenario = ScenarioLoader.lireScenario("pokemon_appli_data/scenario_0.txt");
+            System.out.println(scenario);
+        } catch (Exception e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }*/
+
+
           
 
     }
