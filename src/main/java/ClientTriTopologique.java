fe6d45d5eb224feada9808fa3dcd88f4c3369c62
Andres <andreskpognon@gmail.com>
2025-05-27 20:42:17 +0200

    fin du premier algo

--- src/main/java/ClientTriTopologique.java
+++ src/main/java/ClientTriTopologique.java
@@ -1,31 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileNotFoundException;
 
-public class ClientTriTopologique {
-    public static void main(String[] args) {
-        // 1. Création de l'objet VoisinsTab
-        VoisinsTab voisins = new VoisinsTab();
-
-        // 2. Charger les distances depuis un fichier
-        String cheminFichier = "pokemon_appli_data\\distances.txt"; // Assure-toi que ce fichier est dans le bon dossier
-        voisins.lireDistances(cheminFichier);
-
-        // 3. Initialiser TriTopologique
-        TriTopologique tri = new TriTopologique();
-
-        // 4. Initialiser les variables dans TriTopologique
-        tri.ChListeVille = voisins;
-        tri.ChVilleDeDepart = voisins;
-        tri.ChVilleViste = new ArrayList<>();
-
-        // 5. Appeler la méthode ExploV2
-        tri.ExploV2();
-
-        // 6. Afficher la liste des villes visitées après ExploV2
-        System.out.println("Villes visitées : " + tri.ChVilleViste);
-    }
-
+public static void main(String[] args) throws FileNotFoundException {
+        TriTopologique topo = new TriTopologique();
+        topo.LePlusProche();
 
 }
--- src/main/java/TriTopologique.java
+++ src/main/java/TriTopologique.java
@@ -9,54 +9,13 @@ public class TriTopologique {
     public VoisinsTab ChListeVille;
     VoisinsTab ChVilleDeDepart;
 
-
-    
-    //TODO 
-    private String ChVilleDeDepartTMP;
-
     public TriTopologique() {
         this.ChVilleViste = new ArrayList<>();
         this.ChListeVille = null; // à définir depuis l'extérieur via setListeVille
         // this.ChVilleDeDepart.getIndexMap().get(ChListeVille);
-      
-        
     }
 
 
-    public void ExploV2(){
-        int setupValue = 9999;
-        int indexV2 = 0;
-        int DistanceKilometre = 0;
-        String idk = null; //nom de la Prochaine Ville
-        ArrayList<String> villevue;
-
-        List<Integer> distances = ChVilleDeDepart.getTabDistance().get("Velizy");
-        Map<String, Integer> indexMAP = ChVilleDeDepart.getIndexMap();
-
-        ChVilleViste.add("Velizy");
-
-        for (int i=0 ; i < distances.size(); i++){
-            if (distances.get(i) < setupValue &&  distances.get(i) != 0){
-                setupValue = distances.get(i);
-                indexV2 = i;
-            }
-            DistanceKilometre = DistanceKilometre + distances.get(i); //Finish Distance Calculation
-        }
-
-        for (Entry<String, Integer> entry : indexMAP.entrySet()) {
-            if (entry.getValue() == indexV2) {
-                idk = entry.getKey();  // Found the city name
-                ChVilleViste.add(idk);
-            }
-
-    }   
-
-
-    }
-public static void main(String[] args) throws FileNotFoundException {
-        TriTopologique topo = new TriTopologique();
-        topo.LePlusProche();
-}
     public void LePlusProche() throws FileNotFoundException {
 
         // Load le Scenario
