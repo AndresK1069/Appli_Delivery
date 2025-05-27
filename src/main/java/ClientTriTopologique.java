c0a5aef90db5918f87f64f016fb336f637548bb6
Andres <andreskpognon@gmail.com>
2025-05-27 20:45:39 +0200

    clean up

--- src/main/java/ClientTriTopologique.java
+++ src/main/java/ClientTriTopologique.java
@@ -3,5 +3,4 @@ import java.io.FileNotFoundException;
 public static void main(String[] args) throws FileNotFoundException {
         Algo_0_LePlusProche topo = new Algo_0_LePlusProche();
         topo.LePlusProche();
-
 }
